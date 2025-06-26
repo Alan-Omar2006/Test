92d8f8d1c271461fdf8aafef74ecd41779ea9499
Alan-Omar2006 <martinezhernandezalanomar5@gmail.com>
2025-06-25 21:57:19 -0600

    Cambios en las propiedades del formulario

--- frmPrincipal.Designer.cs
+++ frmPrincipal.Designer.cs
@@ -115,7 +115,11 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
